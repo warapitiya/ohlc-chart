eb9d2a710d67e832d9470d27b003d3b8823c1d66
Malindu Warapitiya <warapitiya93@gmail.com>
2019-05-01 16:32:04 +0800

    feat: 🎸 add initial files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import {hot} from 'react-hot-loader/root'
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>OHCL</h1>
+      </div>
+    );
+  }
+}
+
+export default hot(App);
