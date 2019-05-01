42d0926686b6c3a84d6be593a31af824f99769e7
Malindu Warapitiya <warapitiya93@gmail.com>
2019-05-01 16:47:44 +0800

    refactor: 💡 update pkg version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from "react";
+import Header from './components/header/header';
+
 import {hot} from 'react-hot-loader/root'
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>OHCL</h1>
+      <div className="container mt-5">
+        <Header title={"Name"}></Header>
+        <button className="btn btn-primary">dddd</button>
       </div>
     );
   }
---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+/**
+ * Created by warapitiya on 2019-05-01.
+ */
+import React from 'react';
+
+function Header({title}) {
+  return (
+    <div className="title-container">
+      <h1>{title}</h1>
+    </div>
+  )
+}
+
+export default Header;
