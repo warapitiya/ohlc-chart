4cf51d8a0bf89da322335d01aaaa74a0fc4f8cc9
Malindu Warapitiya <warapitiya93@gmail.com>
2019-05-03 23:25:04 +0800

    fix: 🐛 handle API error

--- src/containers/ChatContainer/ChatContainer.js
+++ src/containers/ChatContainer/ChatContainer.js
@@ -42,6 +42,10 @@ function ChatContainer() {
     {
       "name": "Starbucks",
       "symbol": "SBUX",
+    },
+    {
+      "name": "Comcast",
+      "symbol": "CMCSA",
     }
   ];
 
@@ -51,6 +55,8 @@ function ChatContainer() {
   };
 
   const [stocks, setStocks] = useState([]);
+  const [apiError, setApiError] = useState(false);
+  const [scale, setScale] = useState(10);
   const [selectedStockIndex, setSelectedStockIndex] = useState(getIndexFromStore());
 
 
@@ -58,18 +64,23 @@ function ChatContainer() {
     fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${_stocksList[selectedStockIndex].symbol}&apikey=6N56JX4UJRNSO28E`)
       .then((response) => response.json())
       .then((payload) => {
-        const data = payload['Time Series (Daily)'];
-        const _d = Object.keys(data)
-          .slice(0, 40)
-          .sort((a, b) => new Date(a) - new Date(b))
-          .map((key) => ([
-              new Date(key).toJSON(),
-              parseFloat(data[key]['1. open']),
-              parseFloat(data[key]['2. high']),
-              parseFloat(data[key]['3. low']),
-              parseFloat(data[key]['4. close'])]
-          ));
-        setStocks(_d);
+        if (!payload.hasOwnProperty('Note')) {
+          setApiError(false);
+          const data = payload['Time Series (Daily)'];
+          const _d = Object.keys(data)
+            .slice(0, 40)
+            .sort((a, b) => new Date(a) - new Date(b))
+            .map((key) => ([
+                new Date(key).toJSON(),
+                parseFloat(data[key]['1. open']),
+                parseFloat(data[key]['2. high']),
+                parseFloat(data[key]['3. low']),
+                parseFloat(data[key]['4. close'])]
+            ));
+          setStocks(_d);
+        } else {
+          setApiError(true);
+        }
       });
   }, [selectedStockIndex]);
 
@@ -78,13 +89,53 @@ function ChatContainer() {
     localStorage.setItem(STORAGE_INDEX, index);
   };
 
+  const onScaleChange = (event) => {
+    setScale(parseInt(event.target.value));
+  };
+
   return (
     <div className="row">
       <div className="col-md-3">
         <ListGroup onSelect={onStockSelect} selectedIndex={selectedStockIndex} stockItems={_stocksList}/>
       </div>
       <div className="col-md-9 py-2">
-        {stocks && stocks.length !== 0 && (<OHLCChart scale={10} columns={stocks}/>)}
+        <div className="row">
+          <div className="col-md-6">
+            <div className="border p-2 m-2">
+              <div className="form-check">
+                <input onChange={onScaleChange}
+                       className="form-check-input"
+                       type="radio"
+                       name="scaleRadios"
+                       id="scaleRadio1"
+                       checked={scale === 10}
+                       value="10"/>
+                <label className="form-check-label" htmlFor="exampleRadios1">
+                  Scale 10
+                </label>
+              </div>
+              <div className="form-check">
+                <input onChange={onScaleChange}
+                       className="form-check-input"
+                       type="radio"
+                       name="scaleRadios"
+                       id="scaleRadios2"
+                       checked={scale === 20}
+                       value="20"/>
+                <label className="form-check-label" htmlFor="exampleRadios2">
+                  Scale 20
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            {apiError && (
+              <p className="text-danger">API ERROR: Standard API call frequency is 5 calls per minute
+                and 500 calls per day. Please wait awhile and try again. (https://www.alphavantage.co)</p>
+            )}
+          </div>
+        </div>
+        {stocks && stocks.length !== 0 && (<OHLCChart scale={scale} columns={stocks}/>)}
       </div>
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -31,14 +31,7 @@ module.exports = {
   resolve: {extensions: ["*", ".js", ".jsx"]},
   output: {
     path: path.resolve(__dirname, "dist/"),
-    // publicPath: "/dist/",
     filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "dist/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
   },
   plugins: [
     new HtmlWebpackPlugin({
